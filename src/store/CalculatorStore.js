faaba7fad11d5e27e3370bc0d9d38abd409ee0ea
kate.koppel <katie.spb@gmail.com>
2018-11-18 21:20:41 +0200

    fixes

--- src/store/CalculatorStore.js
+++ src/store/CalculatorStore.js
@@ -9,9 +9,9 @@ class CalculatorStore {
   operators = ['+', '-', '*', '/', '%']
   currentSymbol = "" // last entered symbol
   zeroFlag = 0; // if number starts with zero
-  onlyNumberFlag = 1; // don't show result if it's only one number
-  maxNum = 20; // maximum number of digits on the screen
-  minusFlag = -1; // if '+/-' button was clicked index of "minus", not clicked '-1'
+  onlyNumberFlag = 1; // don't show result if it's only one number on the screen
+  maxNum = 21; // maximum number of digits on the screen
+  minusIndex = -1; // if '+/-' button was clicked before index of "(-", not clicked '-1'
 
   @action clickButton = (value) => {
     switch (true) {
@@ -21,6 +21,7 @@ class CalculatorStore {
         this.result = ""
         this.currentSymbol = "1";
         this.onlyNumberFlag = 1;
+        this.minusIndex = -1;
         break;
 
       // clear
@@ -29,9 +30,11 @@ class CalculatorStore {
         this.result = "";
         this.currentSymbol = "";
         this.onlyNumberFlag = 1;
+        this.minusIndex = -1;
         break;
 
-      case (this.screen.length >= this.maxNum && value !== "+/-"):
+      // if there is no place on the screen
+      case (this.screen.length >= this.maxNum && !(value === "+/-" && this.minusIndex !== -1)):
         break;
 
       // number
@@ -40,7 +43,7 @@ class CalculatorStore {
           this.screen = this.screen.slice(0, this.screen.length - 1);
         }
         this.addToScreen(value);
-        this.result = !this.onlyNumberFlag ? this.evalScreen() : "";
+        this.result = this.evalScreen();
         this.currentSymbol = value;
         this.zeroFlag = (value === '0' && (this.operators.includes(this.currentSymbol) || this.currentSymbol === "")) ? 1 : 0;
         break;
@@ -52,70 +55,79 @@ class CalculatorStore {
         this.currentSymbol = value;
         this.zeroFlag = 0;
         this.onlyNumberFlag = 0;
-        this.minusFlag = -1;
+        this.minusIndex = -1;
         break;
 
 
       // point or %
       case ((value === ".") && this.numbers.includes(this.currentSymbol)):
         this.screen += value;
-        this.result = !this.onlyNumberFlag ? this.evalScreen() : "";
+        this.result = this.evalScreen();
         this.currentSymbol = value;
         break;
 
       // +/-
       case (value === "+/-"):
-        let lastSymbolIndex = this.screen.length - 1;
-        // switch to minus
-        if (this.minusFlag === -1 && this.screen.length <= (this.maxNum - 2)) {
-          let i = lastSymbolIndex;
-          // Going over the string, looking for the first operator
-          while (!this.operators.includes(this.screen[i]) && i !== 0) {
-            i--;
+        // add minus
+        if (this.minusIndex === -1) {
+          // if not enough space
+          if (this.screen.length > (this.maxNum - 3)) {
+            break;
           }
-          // there is operator in the string before ...+135345
-          if (i !== 0) {
+          // if screen was empty or it had only one number
+          else if (this.screen.length === 0 || this.onlyNumberFlag) {
+            this.screen = "(-" + this.screen + ")";
+            this.minusIndex = 0;
+          }
+          // if screen ends with operator
+          else if (this.operators.includes(this.screen[this.screen.length - 1])) {
+            this.minusIndex = this.screen.length;
+            this.screen += "(-)";
+          }
+          else {
+            let i = this.screen.length - 1;
+            // Going over the string backwards, looking for the first operator 
+            // after which to put parentheses
+            while (!this.operators.includes(this.screen[i]) && i !== 0) {
+              i--;
+            }
             let lastNumber = this.screen.slice(i + 1, this.screen.length);
             this.screen = this.screen.slice(0, i + 1) + "(-" + lastNumber + ")";
+            this.minusIndex = i + 1;
+            this.result = this.evalScreen();
           }
-          // no operator, only one number
-          else {
-            this.screen = "(-" + this.screen + ")";
-          }
-          this.minusFlag = i;
         }
         // remove minus
         else {
-          let i = this.minusFlag;
-          if (i !== 0) {
-            let first = this.screen.slice(0, i + 1);
-            let second = this.screen.slice(i + 3, this.screen.length);
+          if (this.minusIndex !== 0) {
+            let first = this.screen.slice(0, this.minusIndex);
+            let second = this.screen.slice(this.minusIndex + 2, this.screen.length);
             second = second.replace(")", "");
             this.screen = first + second;
           }
           else {
             this.screen = this.screen.replace(/(\(-|\))/g, "");
           }
-          this.minusFlag = -1;
+          this.minusIndex = -1;
+          this.result = this.numbers.includes(this.currentSymbol) ? this.evalScreen() : "";
         }
-        this.result = !this.onlyNumberFlag && this.minusFlag !== lastSymbolIndex ? 
-        this.evalScreen() : "";
         break;
-
       default: ;
     }
 
   }
 
   evalScreen() {
+    if (this.onlyNumberFlag) {
+      return "";
+    }
     let screen = (this.eval(this.screen)).toString();
-    console.log(screen)
     return screen.length <= this.maxNum ? screen : "Number too big";
   }
 
   eval(str) {
     try {
-      let result = math.eval(str.replace(/%/g, "/100*"));
+      let result = math.eval(str.replace(/%/g, "/100*")); // for %
       return result;
     }
     catch {
@@ -124,7 +136,7 @@ class CalculatorStore {
   }
 
   addToScreen(value) {
-    if (this.minusFlag === -1) {
+    if (this.minusIndex === -1) {
       this.screen += value;
     }
     else {
